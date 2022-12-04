885c657e9c6ec0a611d2fb012906c833d8d0fd2b
winzoque <114077069+winzoque@users.noreply.github.com>
2022-12-04 15:13:07 +0800

    Delete Home Assignment. Variables & Data Types directory

--- Home Assignment. Variables & Data Types/SavingApp.js
+++
@@ -1,7 +0,0 @@
-let goal = 10000;
-let savings = 7500;
-
-let percentsavings = savings / goal;
-let percentremainings = (1-percentsavings) * 100;
-
-console.log(`Thank you for your discipline and hardwork, Sam! You are now ${percentremainings}% away from your goal of saving ₱${goal}.`);
