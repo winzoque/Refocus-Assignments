7ab966d552ef6f070d07d9e99727afe020d4796c
winzoque <114077069+winzoque@users.noreply.github.com>
2022-12-04 14:39:55 +0800

    Delete HA_Task_StringMethods.js

--- HA_Task_StringMethods.js
+++
@@ -1,7 +0,0 @@
-
-function capsDetector(word){
-    word = word.replace(/[a-z]/g, "").trim().charAt(0);
-    console.log(word);
-}
-
-capsDetector("there is a storm coming to tHe east of the Philippines");
