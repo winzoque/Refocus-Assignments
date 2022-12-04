30f5acf0322d0503cc7a97c1b5631448317603c7
winzoque <114077069+winzoque@users.noreply.github.com>
2022-12-04 15:12:59 +0800

    Delete Home Assignment. Operators & Functions directory

--- Home Assignment. Operators & Functions/TipCalculator.js
+++
@@ -1,7 +0,0 @@
-function computeTip(totalBill){
-    let tip = totalBill * 0.1;
-    let decimal = tip.toFixed(2);
-    return decimal;
-}
-
-console.log("The Tip is ₱" + computeTip(549) + ", Thank you!");
